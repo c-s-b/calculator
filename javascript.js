7eacf83a8447695f159427a39bf902113c879ec0
Casey Brock <casey.brock613@gmail.com>
2022-10-26 22:28:11 -0400

    Create divide Function to Return Quotient of Passed Arguements -test in console

--- javascript.js
+++ javascript.js
@@ -1,17 +1,23 @@
-function add(...numbers) {
+function add( ...numbers ) {
     let sum = 0;
-    numbers.forEach(number => sum += number);
+    numbers.forEach( number => sum += number );
     return sum;
 };
 
-function subtract(...numbers) {
-    const difference = numbers.reduce((previousNumber, nextNumber) => 
-        previousNumber - nextNumber);
+function subtract( ...numbers ) {
+    const difference = numbers.reduce( (previousNumber, nextNumber) => 
+        previousNumber - nextNumber );
     return difference;
 };
 
-function multiply(...numbers) {
+function multiply( ...numbers ) {
     let product = 1;
-    for(const number of numbers) product = product * number;
+    for( const number of numbers ) product = product * number;
     return product;
 };
+
+function divide( ...numbers ) {
+    const quotient = numbers.reduce((previousNumber, nextNumber) =>
+        previousNumber/nextNumber );
+    return quotient;
+}
