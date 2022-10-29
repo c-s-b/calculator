8929dff553120e7cbdd9f8b87ec2072eb523ee33
Casey Brock <casey.brock613@gmail.com>
2022-10-29 12:13:20 -0400

    Create clear and backspace Functions

--- javascript.js
+++ javascript.js
@@ -38,39 +38,67 @@ function operate( number1 = null, operator , number2 = null ) {
     
 }
 
-function clickAButton (previousResult) {
+
+function clickAButton (previousResult = 0) {
     const numberButton = [...document.querySelectorAll(".number-button")];
     const operatorButton = [...document.querySelectorAll(".operator-button")];
-    const equals = document.querySelector(".equals");
+    const equalsButton = document.querySelector(".equals");
     const display = document.querySelector(".result");
+    const clearButton = document.querySelector(".clear");
+    const backspaceButton = document.querySelector(".backspace");
+    const decimalButton = document.querySelector(".decimal");
     let incompleteNumber = [];
     let operation = [];
     let number = previousResult;
 
+    clearButton.addEventListener("click", () => clear());
+
+    backspaceButton.addEventListener("click", () => backspace(number))
+
     numberButton.forEach(button => button.addEventListener("click", () => {
         incompleteNumber.push(button.textContent); //textContent is the number listed on each button
-        number = parseInt(incompleteNumber.join(""));
+        number = parseFloat(incompleteNumber.join(""));
         display.textContent = number;
     }));
 
+    
     operatorButton.forEach(button => button.addEventListener("click", () => {
         operation.push(number);
         if(operation.length < 2 ) operation.push(button.textContent); //textContent is the operator listed on each button
-        number = 0;
         incompleteNumber = [];
     }));
 
-    equals.addEventListener("click", () => {
+    equalsButton.addEventListener("click", () => {
         operation.push(number);
         operation = [displayResult(operation)];
         incompleteNumber = [];
-    });
+    });  
+
 }
 
 function displayResult(operation) {
     result = operate(operation[0], operation[1], operation[2]);
     document.querySelector(".result").textContent = result;
     clickAButton(result);
+}
+
+function clear() {
+    document.querySelector(".result").textContent = 0;
+    clickAButton(0);
+}
+
+function backspace(number) {
+    const display = document.querySelector(".result");
+    let newNumber = 0;
+    const removeLastDigit = Array.from(String(number));
+    removeLastDigit.pop();
+
+    if(removeLastDigit.length > 0){
+    newNumber = parseFloat(removeLastDigit.join(""));
+    } //prevents function from returning NaN
+
+    display.textContent = newNumber;
+    clickAButton(newNumber);
 }
 
 
